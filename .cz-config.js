d47ce643fbbec30f80846996a32cd9fb0f0c9de1
hardingcheng <hardingcheng@163.com>
2021-11-17 17:03:38 +0800

    feat(git hook): husky + commitlint 检查提交描述是否符合规范要求

--- .cz-config.js
+++ .cz-config.js
@@ -48,7 +48,7 @@ module.exports = {
     subject: '请简要描述提交(必填):',
     body: '请输入详细描述(可选):',
     footer: '请输入要关闭的issue(可选):',
-    confirmCommit: '确认使用以上信息提交？(y/n/e/h)'
+    confirmCommit: '确认使用以上信息提交？(y/n)'
   },
   // 跳过问题
   skipQuestions: ['body', 'footer'],
