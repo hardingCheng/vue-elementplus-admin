03d195873db7429b15f0b1e2431b2c8beda01421
hardingcheng <hardingcheng@163.com>
2021-11-18 16:05:08 +0800

    feat(svg): 处理svg图标,内部/外部/element-icon

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,13 +16,14 @@ module.exports = {
   },
   // 需要修改的启用规则及其各自的错误级别
   /**
-     * 错误级别分为三种：
-     * "off" 或 0 - 关闭规则
-     * "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出)
-     * "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
-     */
+   * 错误级别分为三种：
+   * "off" 或 0 - 关闭规则
+   * "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出)
+   * "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
+   */
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'eol-last': 0
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,17 @@ import {
   createWebHashHistory
 } from 'vue-router'
 
-const routes = []
+/**
+ * 公开路由表
+ */
+const publicRoutes = [{
+  path: '/login',
+  component: () => import('@/views/login/index')
+}]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes: publicRoutes
 })
 
 export default router
