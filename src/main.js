3d57c7bfa0960033d60dc79abffce22af06c1f2a
hardingcheng <hardingcheng@163.com>
2021-11-21 08:52:23 +0800

    feat(国际化): 国际化实现原理和配置

--- src/main.js
+++ src/main.js
@@ -16,7 +16,9 @@ import './styles/index.scss'
 import installIcons from './icons'
 // 导入路由鉴权
 import './permission'
+// i18n （PS：导入放到 APP.vue 导入之前，因为后面我们会在 app.vue 中使用国际化内容）
+import i18n from '@/i18n'
 const app = createApp(App)
 installElementPlus(app)
 installIcons(app)
-app.use(store).use(router).mount('#app')
+app.use(i18n).use(store).use(router).mount('#app')
--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,12 +1,27 @@
 // 样式的改变总是由数据进行驱动
+import {
+  LANG
+} from '@/constant'
+import {
+  getItem,
+  setItem
+} from '@/utils/storage'
 export default {
   namespaced: true,
   state: () => ({
-    sidebarOpened: true
+    sidebarOpened: true,
+    language: getItem(LANG) || 'zh'
   }),
   mutations: {
     triggerSidebarOpened (state) {
       state.sidebarOpened = !state.sidebarOpened
+    },
+    /**
+     * 设置国际化
+     */
+    setLanguage (state, lang) {
+      setItem(LANG, lang)
+      state.language = lang
     }
   },
   actions: {}
