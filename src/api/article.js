3091cfde67cdb7b088f7edb0e41ae8f414534422
hardingcheng <hardingcheng@163.com>
2022-01-15 18:22:14 +0800

    feat(文章创建): 创建文章;markdown 编辑器：tui.editor;富文本编辑器：wangEditor;编辑器国际化;

--- src/api/article.js
+++ src/api/article.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-15 13:27:10
+ * @LastEditTime: 2022-01-15 17:58:35
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /vue-elementplus-admin/src/api/article.js
+ */
 import request from '@/utils/request'
 
 /**
@@ -34,5 +42,25 @@ export const deleteArticle = (articleId) => {
 export const articleDetail = (articleId) => {
   return request({
     url: `/article/${articleId}`
+  })
+}
+/**
+ * 创建文章
+ */
+export const createArticle = (data) => {
+  return request({
+    url: '/article/create',
+    method: 'POST',
+    data
+  })
+}
+/**
+ * 编辑文章详情
+ */
+export const articleEdit = (data) => {
+  return request({
+    url: '/article/edit',
+    method: 'POST',
+    data
   })
 }
--- src/views/article-detail/index.vue
+++ src/views/article-detail/index.vue
@@ -19,7 +19,7 @@
 </template>
 <script setup>
 import { ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { articleDetail } from '@/api/article'
 
 // 获取数据
@@ -30,6 +30,12 @@ const getArticleDetail = async () => {
   detail.value = await articleDetail(articleId)
 }
 getArticleDetail()
+
+// 编辑
+const router = useRouter()
+const onEditClick = () => {
+  router.push(`/article/editor/${articleId}`)
+}
 </script>
 
 <style lang="scss" scoped>
